89c896e00e4a58c5a8d489192024fd5103a4b006
MisterCage <mohammad.barbarji@icloud.com>
2025-01-07 22:30:41 +0400

    Changed it to use async and await keywords instead of promises

--- src/index.js
+++ src/index.js
@@ -5,17 +5,30 @@ const img = document.querySelector("img");
 const submitBtn = document.querySelector(".submitBtn");
 const input = document.getElementById("search");
 
+async function getGIF(url) {
+  try {
+    const fetchurl = await fetch(url);
+    const gifData = await fetchurl.json();
+    if (gifData.data) img.src = gifData.data.images.original.url;
+    else img.src = errorImg;
+  } catch (error) {
+    img.src = errorImg;
+  }
+}
 submitBtn.addEventListener("click", (event) => {
   event.preventDefault();
   let searchterm = input.value;
   let url =
     "https://api.giphy.com/v1/gifs/translate?api_key=2vr5FWVFZkqW0qqaZgaRJawYyrvQWLVA&s=" +
     searchterm;
-  fetch(url, { mode: "cors" })
-    .then((response) => response.json())
-    .then((response) => {
-      if (response.data) img.src = response.data.images.original.url;
-      else img.src = errorImg;
-    })
-    .catch((error) => (img.src = errorImg));
+  getGIF(url);
+
+  // fetch(url, { mode: "cors" })
+  //   .then((response) => response.json())
+  //   .then((response) => {
+  //     if (response.data) img.src = response.data.images.original.url;
+  //     else img.src = errorImg;
+  // //   })
+
+  //   .catch((error) => (img.src = errorImg));
 });
